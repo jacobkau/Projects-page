3bb6b80e56cd24aad0d8a99a5b4ded8e227bfd39
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-06-11 14:52:03 +0300

    Remove database credentials from conn.php
    Removed sensitive database credentials from conn.php.

--- voting/includes/conn.php
+++ voting/includes/conn.php
@@ -2,10 +2,10 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$servername = "sql308.infinityfree.com";
-$username = "if0_38412078";
-$password = "g4fqHIbzkrYxxT";
-$dbname = "if0_38412078_witty";
+$servername = " ";
+$username = " ";
+$password = " ";
+$dbname = " ";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
